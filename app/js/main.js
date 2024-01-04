e456f9a33156c8c27b2a4f6343d3061844609d5d
molexandev01~ <molexandev@gmail.com>
2024-01-04 18:34:13 +0200

    added header and slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
-$(function(){
-   
-})
+$(function () {
+    $(".slicer__inner").slick({
+        infinite: true,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        draggable: true,
+    });
+});
