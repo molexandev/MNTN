8df659f70a2a5fed1f9b49f33477f3724d2eff0c
molexandev01~ <molexandev@gmail.com>
2024-01-09 19:30:33 +0200

    added adaptiv for slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $(function () {
     // Слайдер
 
-    $(".slicer__inner").slick({
+    $(".slider__inner").slick({
         infinite: true,
         arrows: false,
         dots: true,
@@ -27,3 +27,47 @@ $(function () {
         }
     });
 });
+
+// Функція для прокрутки сторінки наверх
+function scrollToTop() {
+    // Прокрутити сторінку до верху з плавністю
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+}
+
+// Функція для прокрутки сторінки наверх
+function scrollToTop() {
+    // Прокрутити сторінку до верху з плавністю
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+}
+
+// Отримати елемент кнопки
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Додати обробник подій для події прокрутки
+window.addEventListener("scroll", function () {
+    // Визначити, чи користувач почав скролити сторінку
+    if (
+        document.body.scrollTop > 93 ||
+        document.documentElement.scrollTop > 93
+    ) {
+        scrollToTopBtn.style.display = "flex";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+});
+
+// wow animation
+wow = new WOW({
+    boxClass: "wow",
+    animateClass: "animate__animated",
+    offset: 75,
+    mobile: true,
+    live: true,
+});
+wow.init();
